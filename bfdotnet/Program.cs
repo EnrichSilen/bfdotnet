68ab36e0bfdf0c223fad18aaa1a8724ebd950137
Jan Novák <enrich.silen@gmail.com>
2018-06-24 14:56:39 +0200

    Methods template
    Methods that will be implemented

--- bfdotnet/Program.cs
+++ bfdotnet/Program.cs
@@ -4,9 +4,46 @@ namespace bfdotnet
 {
     class Program
     {
+        byte[] workspace;
+        int spacePointer;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello Brainfuck");
+
+
+
+
+            Console.ReadLine();
         }
+
+        internal static char[] loadSource()
+        {
+            //TODO: loading source from file
+            return new char[1];
+        }
+
+        internal static void Initialisation(int workspaceSize)
+        {
+            //TODO: Initialise all necessary components.
+        }
+
+        internal static string prepareProgram()
+        {
+            //TODO: preperation of program for execution
+            return "";
+        }
+
+        internal static void ArgsHandler(string[] args)
+        {
+            //TODO: handling params and program flow
+        }
+
+        internal static void ExecuteProgram(string program)
+        {
+            //BF program execution
+        }
+
     }
 }
